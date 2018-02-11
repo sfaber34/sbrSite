a7a0c0aef5571fc88daa759a66d386a4e2755851
sfaber34 <sfaber34@gmail.com>
2018-02-11 13:06:12 -0700

    map and directions link done

--- js/mapB.js
+++ js/mapB.js
@@ -1,13 +1,46 @@
 // google.maps.event.addDomListener(window, 'load', initMap);
+
+var map;
+var service;
+var infowindow;
 window.onload = function() {
-  var map;
+
+  var pyrmont = {lat: 39.9884327, lng: -105.0983015};
+
   map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 39.98812, lng: -105.0961352},
+    center: pyrmont,
     zoom: 17
   });
-  var marker = new google.maps.Marker({
-    position: {lat: 39.98812, lng: -105.0961352},
-    map: map,
-    animation: google.maps.Animation.DROP
-  });
+
+  infowindow = new google.maps.InfoWindow();
+  var service = new google.maps.places.PlacesService(map);
+  service.nearbySearch({
+    location: pyrmont,
+    radius: 20,
+    name: 'south boulder road liquor'
+  }, callback);
 };
+
+function callback(results, status) {
+  if (status === google.maps.places.PlacesServiceStatus.OK) {
+    for (var i = 0; i < results.length; i++) {
+      createMarker(results[i]);
+    }
+  }
+}
+
+function createMarker(place) {
+  var placeLoc = place.geometry.location;
+  var marker = new google.maps.Marker({
+    map: map,
+    position: place.geometry.location
+  });
+
+  infowindow.setContent(place.name);
+  infowindow.open(map, marker);
+
+  google.maps.event.addListener(marker, 'click', function() {
+
+    launchMap();
+  });
+}
--- js/theme.js
+++ js/theme.js
@@ -736,3 +736,14 @@ $(function () {
 
 
 });
+
+function launchMap() {
+  if /* if we're on iOS, open in Apple Maps */
+    ((navigator.platform.indexOf("iPhone") != -1) ||
+     (navigator.platform.indexOf("iPod") != -1) ||
+     (navigator.platform.indexOf("iPad") != -1))
+    window.open("http://maps.apple.com/?daddr=south+boulder+road+liquor+lafayette+co");
+
+  else /* else use Google */
+    window.open("https://www.google.com/maps/dir/?api=1&query=south+boulder+road+liquor+lafayette+co&destination=south+boulder+road+liquor+lafayette+co");
+}
